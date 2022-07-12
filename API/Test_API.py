1eec4f24fe9b4c405d40544bbfdd4491f985d3ee
yamdereneko <695494325@qq.com>
2022-07-12 22:16:59 +0800

    Changes

--- API/Test_API.py
+++ API/Test_API.py
@@ -1,21 +1,30 @@
 import json
+
 import requests
 
 
 def request_api(**kwargs):
     action = kwargs.get("action")
+    url = "https://www.jx3api.com/app/{0}".format(action)
+    headers = {'content-type': 'application/json'}
     if action == "daily":
-        url = "https://www.jx3api.com/app/{0}".format(action)
-        headers = {'content-type': 'application/json'}
         response = requests.post(url, headers=headers)
+    elif action == "demon":
+        payload = {
+            "server": "斗转星移"
+        }
+        response = requests.post(url, data=json.dumps(payload), headers=headers)
     else:
         response = None
     return response
 
 
 def main():
-    result = request_api(action="daily")
-    print(result.json())
+    daily = request_api(action="daily")
+    print(daily.json())
+
+    demon = request_api(action="demon")
+    print(demon.json())
 
 
 if __name__ == '__main__':
